0c5e2221083cecc0f988d65ad2bd896d3236ab86
Jared Allard <jaredallard@outlook.com>
2017-01-18 13:40:17 -0800

    make_snapshot: remove stale snapshots

--- devops/make_snapshot.sh
+++ devops/make_snapshot.sh
@@ -7,16 +7,15 @@ out() {
 out "Running in $(pwd)"
 
 pushd "storage"
+
+out "Removing stale snapshots"
+rm -rvf "*.7z"
+
 for file in $(ls);
 do
   SNAPSHOT_FILE="snapshot_$file.7z"
 
   out "snapshot of ${file}"
-
-  if [[ -e "${SNAPSHOT_FILE}" ]]; then
-    out "remove stale snapshot of ${file}"
-    rm -v "${SNAPSHOT_FILE}"
-  fi
 
   7z a "${SNAPSHOT_FILE}" $file 1>/dev/null
   mv -v ${SNAPSHOT_FILE} "../snapshots"
